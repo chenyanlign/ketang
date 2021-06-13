895e1f3aac80f75e710340271e7463f2da73fa2b
mazouri <364243374@qq.com>
2021-06-13 21:35:39 +0800

    first commit

---
+++ src/main/java/com/mazouri/ketangpai/KetangpaiApplication.java
@@ -0,0 +1,16 @@
+package com.mazouri.ketangpai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author mazouri
+ */
+@SpringBootApplication
+public class KetangpaiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KetangpaiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mazouri/ketangpai/KetangpaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mazouri.ketangpai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KetangpaiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
